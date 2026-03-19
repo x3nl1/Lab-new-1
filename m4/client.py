e22763fd950f16c24c7922b4a9a7f9ac9f3176be
x3nl1 <stepanagapov2005@gmail.com>
2026-03-19 23:28:40 +0300

    fix(m4): added logic.go in subproccess.Popen

--- m4/client.py
+++ m4/client.py
@@ -3,11 +3,10 @@ import json
 
 def run(numbers):
     proc = subprocess.Popen(
-        ["go", "run", "main.go"],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        cwd="m4"
-    )
+    ["go", "run", "main.go", "logic.go"],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+)
     data = json.dumps({"numbers": numbers}).encode()
     out, _ = proc.communicate(data)
     return json.loads(out)["sum"]
